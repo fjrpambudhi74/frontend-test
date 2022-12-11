3f5aa18866f313ef00739ffb1c3fa2dec63da506
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-12-11 14:03:58 +0700

    create component input text error

---
+++ project/src/components/BaseInputTextError.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="computedText" class="invalid-input__text">
+    <img :src="iconInfo" alt="" width="13" />
+    <span>{{ computedText }}</span>
+  </div>
+</template>
+
+<script>
+import iconInfo from "@/assets/images/info-red.svg";
+export default {
+  name: "BaseInputError",
+  props: {
+    text: {
+      default: "",
+      type: String,
+    },
+  },
+  data() {
+    return {
+      iconInfo,
+    };
+  },
+  computed: {
+    computedText() {
+      return typeof this.text == "string" ? this.text.trim() : "";
+    },
+  },
+};
+</script>
