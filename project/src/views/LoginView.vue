359c6b60a131923ea07c44772843f893fe008316
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-12-11 12:38:56 +0700

    add styles in login view

--- project/src/views/LoginView.vue
+++ project/src/views/LoginView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="login">Login</div>
+  <div class="login">
+    <div class="login-container">
+      <div class="login-content">
+        <div class="login-box">
+          <a
+            href="https://enterprise.mile.app/"
+            target="_blank"
+            rel="noreferrer noopener"
+            class="login-box--link"
+          >
+            <img
+              src="../assets/images/logo-milesapp.png"
+              alt=""
+              class="login-box--logo"
+            />
+          </a>
+          <div class="login-box-card">
+            <h1 class="login-box-card--title">Login</h1>
+          </div>
+        </div>
+        <div class="login-bg">
+          <img src="../assets/images/bg-2.png" alt="" />
+          <div class="login-footer">
+            &copy; Copyright 2021 PT. Paket Informasi Digital. All Rights
+            Reserved
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
