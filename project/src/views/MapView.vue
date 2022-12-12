10b197d4e49859cbb80cc9d2bc9c0269970353eb
fjrpambudhi74 <fjrpambudhi@hotmail.com>
2022-12-12 12:05:58 +0700

    implement google maps

--- project/src/views/MapView.vue
+++ project/src/views/MapView.vue
@@ -1,5 +1,29 @@
 <template>
-  <div class="about">
+  <div class="map">
     <h1>This is an map page</h1>
+    <GmapMap :center="center" :zoom="12" style="width: 100%; height: 400px" />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      center: { lat: 45.508, lng: -73.587 },
+    };
+  },
+  mounted() {
+    this.geolocate();
+  },
+  methods: {
+    geolocate: function () {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.center = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        };
+      });
+    },
+  },
+};
+</script>
